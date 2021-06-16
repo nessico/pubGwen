e6674ebfa8effd4b09821085a21d2b20f053ea15
Danh <danhpham351@gmail.com>
2021-06-16 18:17:14 -0400

    feat(api): add like other users

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -10,5 +10,30 @@ namespace API.Data
         }
 
         public DbSet<AppUser> Users { get; set; }
+        public DbSet<UserLike> Likes { get; set; }
+
+        //configuring userlikes in EF
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<UserLike>()
+                .HasKey(k => new { k.SourceUserId, k.LikedUserId });
+
+            //configuring keys
+            builder.Entity<UserLike>()
+                .HasOne(s => s.SourceUser)
+                .WithMany(l => l.LikedUsers)
+                .HasForeignKey(s => s.SourceUserId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            builder.Entity<UserLike>()
+                .HasOne(s => s.LikedUser)
+                .WithMany(l => l.LikedByUsers)
+                .HasForeignKey(s => s.LikedUserId)
+                .OnDelete(DeleteBehavior.NoAction);
+        }
+
+
     }
 }
---
+++ API/Entities/UserLike.cs
@@ -0,0 +1,11 @@
+namespace API.Entities
+{
+    //Joint entity for user and userlike
+    public class UserLike
+    {
+       public AppUser SourceUser { get; set; }
+       public int SourceUserId { get; set; }   
+       public AppUser LikedUser { get; set; }
+       public int LikedUserId { get; set; }
+    }
+}
