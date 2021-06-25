9d77fa379698bccca003a62255b1254983b9d081
Danh <danhpham351@gmail.com>
2021-06-25 03:35:40 -0400

    build(core): publish docker, aws

--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -68,12 +68,12 @@ namespace API.Data
                 .Include(p => p.Photos)
                 .SingleOrDefaultAsync(x => x.UserName == username);
         }
-
+        
         public async Task<string> GetUserGender(string username)
         {
             return await _context.Users
-            .Where(x => x.UserName == username)
-            .Select(x => x.Gender).FirstOrDefaultAsync();
+                .Where(x => x.UserName == username)
+                .Select(x => x.Gender).FirstOrDefaultAsync();
         }
 
         public async Task<IEnumerable<AppUser>> GetUsersAsync()
