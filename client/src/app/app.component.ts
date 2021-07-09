875e57a78bce71cac4156a3597f2c8e1dcea8375
Danh <danhpham351@gmail.com>
2021-07-08 21:37:21 -0400

    fix(client): add null validator for login

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -3,6 +3,9 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { User } from './_models/user';
 import { AccountService } from './_services/account.service';
+import { environment } from 'src/environments/environment';
+import { IProduct } from './_models/product';
+import { IProductPagination } from './_models/productPagination';
 
 @Component({
   selector: 'app-root',
@@ -12,13 +15,28 @@ import { AccountService } from './_services/account.service';
 export class AppComponent implements OnInit {
   title = 'Gwen';
   users: any;
+  baseUrl = environment.apiUrl;
+  products!: IProduct[];
 
   constructor(
     private accountService: AccountService,
-    private presence: PresenceService
+    private presence: PresenceService,
+    private http: HttpClient
   ) {}
   ngOnInit() {
     this.setCurrentUser();
+    // this.http
+    //   .get<IProductPagination>(
+    //     'https://localhost:5001/api/products?pageSize=50'
+    //   )
+    //   .subscribe(
+    //     (response: IProductPagination) => {
+    //       this.products = response.data;
+    //     },
+    //     (error) => {
+    //       console.log(error);
+    //     }
+    //   );
   }
 
   setCurrentUser() {
