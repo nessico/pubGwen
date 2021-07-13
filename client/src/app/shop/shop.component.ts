6957b21f7b89c09c0db6404cacb07d86d72d2d90
Danh <danhpham351@gmail.com>
2021-07-12 20:46:46 -0400

    fix(client): reset pagination on filter, request

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -71,11 +71,13 @@ export class ShopComponent implements OnInit {
 
   onBrandSelected(brandId: number) {
     this.shopParams.brandId = brandId;
+    this.shopParams.pageIndex = 1;
     this.getProducts();
   }
 
   onTypeSelected(typeId: number) {
     this.shopParams.typeId = typeId;
+    this.shopParams.pageIndex = 1;
     this.getProducts();
   }
 
@@ -86,6 +88,7 @@ export class ShopComponent implements OnInit {
 
   onSearch() {
     this.shopParams.search = this.searchTerm.nativeElement.value;
+    this.shopParams.pageIndex = 1;
     this.getProducts();
   }
 
@@ -96,7 +99,10 @@ export class ShopComponent implements OnInit {
   }
 
   onPageChanged(event: any) {
-    this.shopParams.pageIndex = event;
-    this.getProducts();
+    if (this.shopParams.pageIndex !== event) {
+      this.shopParams.pageIndex = event;
+      this.getProducts();
+    }
+
   }
 }
