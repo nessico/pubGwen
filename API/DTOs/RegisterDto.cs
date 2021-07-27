62ccbb7e4f0d6038f5d567f3a5591b2a08e0239b
Danh <danhpham351@gmail.com>
2021-07-27 15:52:03 -0400

    feat(api): update user data annotation, validation

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -5,15 +5,25 @@ namespace API.DTOs
 {
     public class RegisterDto
     {
-        [Required] public string Email { get; set; }
-        [Required] public string Username { get; set; }
-        [Required] public string DisplayName { get; set; }
-        [Required] public string Gender { get; set; }
-        [Required] public DateTime DateOfBirth { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
 
         [Required]
-        [RegularExpression("(?=^.{6,8}$)(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\\s).*$", 
-            ErrorMessage = "Password must have 1 Uppercase, 1 Lowercase, 1 number, 1 non alphanumeric and at least 6 characters")]
+        public string Username { get; set; }
+
+        [Required]
+        public string DisplayName { get; set; }
+
+        [Required]
+        public string Gender { get; set; }
+
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required]
+        [RegularExpression("(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{6,8}",
+            ErrorMessage = "Password must have 1 Uppercase, 1 Lowercase, 1 number, 1 non alphanumeric and between 6 to 8 characters")]
         public string Password { get; set; }
     }
 }
