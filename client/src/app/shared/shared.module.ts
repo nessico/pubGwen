5ba3c42b272757cabba2242b3ca7c0907fabb0d6
Danh <danhpham351@gmail.com>
2021-08-01 22:32:33 -0400

    refactor(client): move input compo for readability

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -16,10 +16,11 @@ import { PagingHeaderComponent } from './components/shopComponents/paging-header
 import { PagerComponent } from './components/shopComponents/pager/pager.component';
 import { CarouselModule } from 'ngx-bootstrap/carousel';
 import { OrderSummaryComponent } from './components/shopComponents/order-summary/order-summary.component';
-import { TextInputComponent } from './text-input/text-input.component';
+import { TextInputComponent } from './components/generalComponents/text-input/text-input.component';
+import { DateInputComponent } from './components/generalComponents/date-input/date-input.component';
 
 @NgModule({
-  declarations: [PagingHeaderComponent, PagerComponent, OrderSummaryComponent, TextInputComponent],
+  declarations: [PagingHeaderComponent, PagerComponent, OrderSummaryComponent, TextInputComponent, DateInputComponent],
   imports: [
     CommonModule,
     BsDropdownModule.forRoot(),
