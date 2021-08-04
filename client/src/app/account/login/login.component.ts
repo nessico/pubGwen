9c924c8b0021168ec46e978835db363c1b4ef61b
Danh <danhpham351@gmail.com>
2021-08-04 01:02:41 -0400

    feat(api): implement CVA into login form

--- client/src/app/account/login/login.component.ts
+++ client/src/app/account/login/login.component.ts
@@ -38,7 +38,7 @@ export class LoginComponent implements OnInit {
     this.loginForm = new FormGroup({
       email: new FormControl('', [
         Validators.required,
-        // Validators.pattern('^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$'),
+        Validators.pattern('^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$'),
       ]),
       password: new FormControl('', Validators.required),
     });
