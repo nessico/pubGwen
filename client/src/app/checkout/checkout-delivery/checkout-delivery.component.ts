e6956e22f37dd9d8e60ae63c2ae28a1d18f70618
Danh <danhpham351@gmail.com>
2021-08-08 00:44:52 -0400

    feat(client): add delivery checkout functions

--- client/src/app/checkout/checkout-delivery/checkout-delivery.component.ts
+++ client/src/app/checkout/checkout-delivery/checkout-delivery.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { IDeliveryMethod } from './../../shared/_models/shopModels/deliveryMethod';
+import { CheckoutService } from './../checkout.service';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-checkout-delivery',
   templateUrl: './checkout-delivery.component.html',
-  styleUrls: ['./checkout-delivery.component.scss']
+  styleUrls: ['./checkout-delivery.component.scss'],
 })
 export class CheckoutDeliveryComponent implements OnInit {
-
-  constructor() { }
+  @Input() checkoutForm!: FormGroup;
+  deliveryMethods!: IDeliveryMethod[];
+  constructor(private checkoutService: CheckoutService) {}
 
   ngOnInit(): void {
+    this.checkoutService.getDeliveryMethods().subscribe(
+      (dm: IDeliveryMethod[]) => {
+        this.deliveryMethods = dm;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
-
 }
--- client/src/app/checkout/checkout.service.ts
+++ client/src/app/checkout/checkout.service.ts
@@ -1,9 +1,23 @@
+import { IDeliveryMethod } from './../shared/_models/shopModels/deliveryMethod';
+import { map } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+import { environment } from './../../environments/environment';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CheckoutService {
+  baseUrl = environment.apiUrl;
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getDeliveryMethods() {
+    return this.http
+      .get<IDeliveryMethod[]>(this.baseUrl + 'orders/deliveryMethods')
+      .pipe(
+        map((dm: IDeliveryMethod[]) => {
+          return dm.sort((a, b) => b.price - a.price);
+        })
+      );
+  }
 }
