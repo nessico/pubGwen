63fa3d41d53ae7709edff6210d3f312afc03ee78
Danh <danhpham351@gmail.com>
2021-08-07 23:41:42 -0400

    fix(api): undo //_ for CORS from prev docs styling

--- API/Startup.cs
+++ API/Startup.cs
@@ -52,7 +52,7 @@ namespace API
             app.UseCors(x => x.AllowAnyHeader()
             .AllowAnyMethod()
             .AllowCredentials()
-            .WithOrigins("https:// localhost:4200"));
+            .WithOrigins("https://localhost:4200"));
 
             app.UseAuthentication();
 
