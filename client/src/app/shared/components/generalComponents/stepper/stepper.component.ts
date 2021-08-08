a2316b8470807e136e95f69e06adc3bcb4fdca45
Danh <danhpham351@gmail.com>
2021-08-07 23:22:48 -0400

    fix(client): re-add linear mode on stepper

--- client/src/app/shared/components/generalComponents/stepper/stepper.component.ts
+++ client/src/app/shared/components/generalComponents/stepper/stepper.component.ts
@@ -10,7 +10,9 @@ import { Component, Input, OnInit } from '@angular/core';
 // https://material.angular.io/guide/creating-a-custom-stepper-using-the-cdk-stepper
 export class StepperComponent extends CdkStepper implements OnInit {
   // Sets child component to linearMode (requires the user to complete previous steps before proceeding)
-  @Input() linearModeSelected!: boolean; 
+  @Input() linearModeSelected!: boolean;
+  ngOnInit(): void {
+    this.linear = this.linearModeSelected;
   }
 
   // Keeps track of what step you are currently on
