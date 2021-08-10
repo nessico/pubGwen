d1541d514f1644139063c4a4b98d8632a15c97b2
Danh <danhpham351@gmail.com>
2021-08-10 05:50:24 -0400

    feat(client): add checkout order submission

--- client/src/app/checkout/checkout-payment/checkout-payment.component.ts
+++ client/src/app/checkout/checkout-payment/checkout-payment.component.ts
@@ -1,15 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { IOrder } from './../../shared/_models/shopModels/order';
+import { BasketService } from './../../basket/basket.service';
+
+import { Component, Input, OnInit } from '@angular/core';
+import { Router, NavigationExtras } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+
+import { CheckoutService } from '../checkout.service';
+import { FormGroup } from '@angular/forms';
+import { IBasket } from 'src/app/shared/_models/shopModels/basket';
 
 @Component({
   selector: 'app-checkout-payment',
   templateUrl: './checkout-payment.component.html',
-  styleUrls: ['./checkout-payment.component.scss']
+  styleUrls: ['./checkout-payment.component.scss'],
 })
 export class CheckoutPaymentComponent implements OnInit {
+  @Input() checkoutForm!: FormGroup;
 
-  constructor() { }
+  constructor(
+    private checkoutService: CheckoutService,
+    private toastr: ToastrService,
+    private router: Router,
+    private basketService: BasketService
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  submitOrder() {
+    const basket = this.basketService.getCurrentBasketValue();
+    const orderToCreate = this.getOrderToCreate(basket!);
+    this.checkoutService.createOrder(orderToCreate).subscribe(
+      <IOrder>(order: IOrder) => {
+        this.toastr.success('Order was successful');
+        this.basketService.deleteLocalBasket(basket!.id);
+        const navigationExtras: NavigationExtras = { state: order };
+        this.router.navigate(['checkout/success'], navigationExtras);
+      },
+      (error) => {
+        this.toastr.error(error.message);
+        console.log(error);
+      }
+    );
   }
 
+  private getOrderToCreate(basket: IBasket) {
+    return {
+      basketId: basket.id,
+      deliveryMethodId: +this.checkoutForm
+        .get('deliveryForm')!
+        .get('deliveryMethod')!.value,
+      shipToAddress: this.checkoutForm.get('addressForm')!.value,
+    };
+  }
 }
