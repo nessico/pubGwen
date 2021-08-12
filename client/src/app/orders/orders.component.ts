d0c32374a8803790074b92fcd120626890255206
Danh <danhpham351@gmail.com>
2021-08-11 20:10:06 -0400

    feat(client): add orders skeleton

--- client/src/app/orders/orders.component.ts
+++ client/src/app/orders/orders.component.ts
@@ -1,15 +1,29 @@
+import { IOrder } from 'src/app/shared/_models/shopModels/order';
+import { OrdersService } from './orders.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-orders',
   templateUrl: './orders.component.html',
-  styleUrls: ['./orders.component.scss']
+  styleUrls: ['./orders.component.scss'],
 })
 export class OrdersComponent implements OnInit {
+  orders!: IOrder[];
 
-  constructor() { }
+  constructor(private orderService: OrdersService) {}
 
   ngOnInit(): void {
+    this.getOrders();
   }
 
+  getOrders() {
+    this.orderService.getOrders().subscribe(
+      (response: any) => {
+        this.orders = response;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
 }
--- client/src/app/orders/orders.service.ts
+++ client/src/app/orders/orders.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { environment } from './../../environments/environment';
 import { Injectable } from '@angular/core';
 
+
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class OrdersService {
+  baseUrl = environment.apiUrl;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getOrders() {
+    return this.http.get(this.baseUrl + 'orders');
+  }
+
+  getOrderDetailed(id: number) {
+    return this.http.get(this.baseUrl + 'orders' + id);
+  }
 }
