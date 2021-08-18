995d76d0db4041e976db800fd0faa4db4ed5611f
Danh <danhpham351@gmail.com>
2021-08-18 01:08:30 -0400

    feat(client): add orders pagination

--- client/src/app/orders/orders.component.ts
+++ client/src/app/orders/orders.component.ts
@@ -1,6 +1,9 @@
+import { UserParams } from './../shared/_models/accountModels/userParams';
 import { IOrder } from 'src/app/shared/_models/shopModels/order';
 import { OrdersService } from './orders.service';
 import { Component, OnInit } from '@angular/core';
+import { IPagination } from '../shared/_models/accountModels/pagination';
+import { ShopParams } from '../shared/_models/shopModels/shopParams';
 
 @Component({
   selector: 'app-orders',
@@ -9,6 +12,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdersComponent implements OnInit {
   orders!: IOrder[];
+  pagination!: IPagination;
+  shopParams = new ShopParams();
+  totalCount!: number;
 
   constructor(private orderService: OrdersService) {}
 
@@ -17,13 +23,25 @@ export class OrdersComponent implements OnInit {
   }
 
   getOrders() {
-    this.orderService.getOrders().subscribe(
+    this.orderService.getOrders(this.shopParams).subscribe(
       (response: any) => {
-        this.orders = response;
+        this.orders = response.data;
+        this.shopParams.pageIndex = response.pageIndex;
+        this.shopParams.pageSize = response.pageSize;
+        this.totalCount = response.count;
+        console.log(this.orders);
+        console.log(this.shopParams);
+        console.log(this.totalCount);
       },
       (error) => {
         console.log(error);
       }
     );
+  }
+  onPageChanged(event: any) {
+    if (this.shopParams.pageIndex !== event) {
+      this.shopParams.pageIndex = event;
+      this.getOrders();
+    }
   }
 }
--- client/src/app/orders/orders.service.ts
+++ client/src/app/orders/orders.service.ts
@@ -1,8 +1,9 @@
-import { HttpClient } from '@angular/common/http';
+import { IProductPagination } from './../shared/_models/shopModels/productPagination';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { environment } from './../../environments/environment';
 import { Injectable } from '@angular/core';
-
-
+import { ShopParams } from '../shared/_models/shopModels/shopParams';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -12,8 +13,21 @@ export class OrdersService {
 
   constructor(private http: HttpClient) {}
 
-  getOrders() {
-    return this.http.get(this.baseUrl + 'orders');
+  getOrders(shopParams: ShopParams) {
+    let params = new HttpParams();
+    params = params.append('pageIndex', shopParams.pageIndex.toString());
+    params = params.append('pageSize', shopParams.pageSize.toString());
+
+    return this.http
+      .get<IProductPagination>(this.baseUrl + 'orders', {
+        observe: 'response',
+        params,
+      })
+      .pipe(
+        map((response) => {
+          return response.body;
+        })
+      );
   }
 
   getOrderDetailed(id: number) {
