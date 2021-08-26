c90c03d7ddcb089aa7bd00638a39b51f00c54e3d
Danh <danhpham351@gmail.com>
2021-08-26 02:36:02 -0400

    feat(api): add paymentsController skeleton

--- API/DTOs/CustomerBasketDto.cs
+++ API/DTOs/CustomerBasketDto.cs
@@ -5,9 +5,18 @@ namespace API.DTOs
 {
     public class CustomerBasketDto
     {
-       [Required]
+        [Required]
         public string Id { get; set; }
 
-        public List<BasketItemDto> Items { get; set; } 
+        public List<BasketItemDto> Items { get; set; }
+
+        // Optional, because they won't have a delivery method until they get to the checkout
+        public int? DeliveryMethodId { get; set; }
+
+        // Used by Stripe to confirm payment intent
+        public string ClientSecret { get; set; }
+
+        // Used to update payment intent in case customer wants to change items in their basket
+        public string PaymentIntentId { get; set; }
     }
 }
