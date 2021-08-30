2ce712358c5b60dc55b7d22d1aed74b2e14d1382
Danh <danhpham351@gmail.com>
2021-08-29 20:26:18 -0400

    fix(client): show stripe card failure msg

--- client/src/app/checkout/checkout-payment/checkout-payment.component.ts
+++ client/src/app/checkout/checkout-payment/checkout-payment.component.ts
@@ -103,7 +103,7 @@ export class CheckoutPaymentComponent implements AfterViewInit, OnDestroy {
               const navigationExtras: NavigationExtras = { state: order };
               this.router.navigate(['checkout/success'], navigationExtras);
             } else {
-              this.toastr.error('Payment Failed');
+              this.toastr.error(result.error.message);
             }
           });
       },
